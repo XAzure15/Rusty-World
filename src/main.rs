16bf2d5dd5bcb738c1000a185b68fdaa70fe3a71
Xrider-15 <171270040+Xrider-15@users.noreply.github.com>
2024-06-01 01:17:08 +0200

    added variables

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
 fn main() {
-    println!("Hello, world!");
+    //println!("Hello, world!");
+    let  x=5;
+    println!("x has the value {}", x);
+    x = 6;
+    println!("The new value of x is: {}", x);
  }
